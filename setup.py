e6ad074a0d8c73f227bbd2d26bb26919e882a704
Sandeep Junnarkar <sjnews@gmail.com>
2023-10-29 17:34:47 -0400

    added comments to set up

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='function1',
+    name='function1', ##name of repo enclosing folder
     version='0.0.2',
     author='Sandeep Junnarkar',
     author_email='sjnews@gmail.com',
@@ -16,6 +16,6 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/sandeepmj/function1/issues"
     },
     license='MIT',
-    packages=['mytools'],
+    packages=['mytools'], ## name of folder that holds the functions
     install_requires=['requests'],
 )
