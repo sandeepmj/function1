ec0c2e92d235e145c6a75493f68e787f322d0a78
Sandeep Junnarkar <sjnews@gmail.com>
2023-10-29 17:21:29 -0400

    fixed points in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='my_functions',
+    name='functions1',
     version='0.0.1',
     author='Sandeep Junnarkar',
     author_email='sjnews@gmail.com',
