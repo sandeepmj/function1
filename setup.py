42a5b7553507343417ef5f2ca74a895023b45e33
Sandeep Junnarkar <sjnews@gmail.com>
2023-10-29 17:57:06 -0400

    v 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='function1', ##name of repo enclosing folder
-    version='0.0.4',
+    version='0.0.5',
     author='Sandeep Junnarkar',
     author_email='sjnews@gmail.com',
     description='Testing installation of Package',
