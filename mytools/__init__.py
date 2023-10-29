2673b5244baab5ff11fc552d260e931e27d1db75
Sandeep Junnarkar <sjnews@gmail.com>
2023-10-29 17:36:31 -0400

    comment init  py

--- mytools/__init__.py
+++ mytools/__init__.py
@@ -1,5 +1,5 @@
 # This is so that you can import ppack or import average from ppack
 # in stead of from ppack.functions import average
 
-
+## .all_functions refers to file that holds all the functions
 from .all_functions import addNumbers, helloPerson
